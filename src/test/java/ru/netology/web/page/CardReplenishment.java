d34827d9e280f18d3669b3e04cb98a59f8ff29d1
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-03-07 19:51:53 +0300

    changed project

--- src/test/java/ru/netology/web/page/CardReplenishment.java
+++ src/test/java/ru/netology/web/page/CardReplenishment.java
@@ -4,16 +4,17 @@ import com.codeborne.selenide.SelenideElement;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class CardReplenishment {
     private SelenideElement replenishment = $("[data-test-id=action-deposit]");
-    private SelenideElement balance = $("[data-test-id=amount]");
+    private SelenideElement sum =  $x("//span[contains(text(),'Сумма')]");
     private SelenideElement card = $("[data-test-id=from]");
     private SelenideElement transfer = $("[data-test-id=action-transfer]");
 
     public DashboardPage validUser (DataHelper.AuthInfo info) {
         replenishment.click();
-        balance.setValue(info.getBalance());
+        sum.setValue("500");
         card.setValue(info.getNumber());
         transfer.click();
         return new DashboardPage();
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -3,6 +3,7 @@ package ru.netology.web.test;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.CardReplenishment;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPageV2;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -23,10 +24,15 @@ class MoneyTransferTest {
     open("http://localhost:9999");
     var cardReplenishment = new CardReplenishment();
     var authInfo = DataHelper.getAuthInfo();
-    var balance = authInfo.getBalance();
+    var sum = ("500");
     var card = authInfo.getNumber();
-
   }
+ // @Test
+  //void shouldTransferMoneyVisible() {
+  / open("http://localhost:9999");
+  // var DashboardPage = new DashboardPage();
 
+
+ // }
 }
 
